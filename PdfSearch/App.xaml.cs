b342952341772225ac5875d7fa7665068b3b0b4c
Tomasz Scislowicz <toomasz@gmail.com>
2025-01-24 17:13:57 +0100

    Add initial project sketch with syncfusion/mvvm framework packages

---
+++ PdfSearch/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using PdfSearch.ViewModels;
+using System.Windows;
+
+namespace PdfSearch;
+
+public partial class App : System.Windows.Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var mainWindow = new MainWindow
+        {
+            DataContext = new MainWindowViewModel()
+        };
+        mainWindow.Show();
+        base.OnStartup(e);
+    }
+}
---
+++ PdfSearch/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,57 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Syncfusion.Pdf;
+using Syncfusion.Windows.Forms.PdfViewer;
+using System.Collections.ObjectModel;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+
+namespace PdfSearch.ViewModels;
+
+internal partial class MainWindowViewModel :ObservableObject
+{
+    [RelayCommand]
+    public void AddPdfFile()
+    {
+        var fileDialog = new OpenFileDialog
+        { 
+            Filter = "PDF Files (*.pdf)|*.pdf", 
+            Multiselect = true
+        };
+        if(fileDialog.ShowDialog() == false)
+        {
+            return;
+        }
+        var pdfFiles = fileDialog.FileNames;
+        foreach(var pdfFile in pdfFiles)
+        {
+            PdfFiles.Add(pdfFile);
+        }
+    }
+
+    [RelayCommand]
+    public void AddCsvFile()
+    {
+        var fileDialog = new OpenFileDialog
+        {
+            Filter = "CSV Files (*.csv)|*.csv",
+            Multiselect = true
+        };
+        if (fileDialog.ShowDialog() == false)
+        {
+            return;
+        }
+        var csvFile = fileDialog.FileName;
+    }
+
+    [RelayCommand]
+    public void Search()
+    {
+        PdfDocumentView pdfDocumentView = new();
+        //Load the PDF file.
+        pdfDocumentView.Load(PdfFiles[0]);
+        string extractedText = pdfDocumentView.ExtractText(0, out TextLines textLines);
+    }
+
+    [ObservableProperty]
+    public ObservableCollection<string> PdfFiles { get; } = new();
+}
